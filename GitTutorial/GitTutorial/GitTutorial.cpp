ed42b283d91fee9373781fbec3400f362da1a004
king082391 <king910823@gmail.com>
2022-03-09 15:05:36 +0800

    test 7B

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -3,8 +3,19 @@
 
 #include <iostream>
 #include <string>
+#include <ctime>
+#include <cstdlib>
 
 bool Guess(int number) {
+	static int target = -1;
+
+	srand(time(NULL));
+	if (target == -1) {
+		target = rand() % 100 + 1;
+	}
+
+	if (number == target)
+		return true;
 	return false;
 }
 int main()
